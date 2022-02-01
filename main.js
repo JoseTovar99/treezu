038d19a8f41ff534aa6e7dc535eb82cb73d9b3cb
JoseTovar510 <tovarjose1013@gmail.com>
2022-01-31 23:05:47 -0800

    More onclick changes

--- main.js
+++ main.js
@@ -9,19 +9,23 @@ let btn3 = document.querySelector('.btnFullScreen3');
 btn1.addEventListener('click', () => {
     img.src = 'img/Camp1.jpg';
     document.getElementById('demo').innerHTML = 'Sequoia National Park';
-    document.getElementById('descChange').innerHTML = 'Huge mountains, rugged foothills, deep canyons, vast caverns, and the worlds largest trees.';
+    document.getElementById('descrChange').innerHTML = 'Huge mountains, rugged foothills, deep canyons, vast caverns, and the worlds largest trees.';
     document.getElementById('ratingChange').innerHTML = '(12.5K)';
 
 })
 
 btn2.addEventListener('click', () => {
     img.src = 'img/Camp5.jpg';
-    
+    document.getElementById('demo').innerHTML = 'Yosemite National Park';
+    document.getElementById('descrChange').innerHTML = 'Located in Californias Sierra Nevada mountains. Famed for its giant, ancient sequoia trees, and for Tunnel View.';
+    document.getElementById('ratingChange').innerHTML = '(39.1K)';
 })
 
 btn3.addEventListener('click', () => {
     img.src = 'img/Camp3.jpeg';
-    
+    document.getElementById('demo').innerHTML = 'Crater Lake National park';
+    document.getElementById('descrChange').innerHTML = 'Crater Lake National Park is in the Cascade Mountains of southern Oregon. Its known for its namesake Crater Lake, formed by the now-collapsed volcano, Mount Mazama and the beautiful trails.';
+    document.getElementById('ratingChange').innerHTML = '(11.8K)';
 })
 
 
